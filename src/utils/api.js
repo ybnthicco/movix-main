3f37da868204d14a3b3051353e22d6e71e08afa7
ybnthicco <75510362+ybnthicco@users.noreply.github.com>
2023-10-21 11:08:15 +0300

    Add files via upload

---
+++ src/utils/api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+
+const headers = {
+    Authorization: "bearer " + '7f4a091146e24dcd04881d585cb3a68e',
+};
+
+export const fetchDataFromApi = async (url, params) => {
+    try {
+        const { data } = await axios.get(BASE_URL + url, {
+            headers,
+            params,
+        });
+        return data;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+};
